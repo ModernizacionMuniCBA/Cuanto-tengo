5675fc98b0afd5f09b0f965017aac81e46aa57db
Gaston Ramirez <gastonframirez@gmail.com>
2017-03-21 17:39:57 -0300

    Agrego archivo que faltaba

---
+++ www/js/fullwModalVersionService.js
@@ -0,0 +1,48 @@
+app.service('fullwModalVersionService', ['$uibModal',
+    function ($uibModal) {
+
+        var modalDefaults = {
+            backdrop: true,
+            keyboard: false ,
+            modalFade: true,
+            templateUrl: '../www/angular-templates/modalInformeVersion.html'
+        };
+
+        var modalOptions = {
+            closeButtonText: 'Close',
+            actionButtonText: 'OK',
+            headerText: 'Proceed?'
+        };
+
+        this.showModal = function (customModalDefaults, customModalOptions) {
+            if (!customModalDefaults) customModalDefaults = {};
+            customModalDefaults.backdrop = 'static';
+            return this.show(customModalDefaults, customModalOptions);
+        };
+        this.show = function (customModalDefaults, customModalOptions) {
+            //Create temp objects to work with since we're in a singleton service
+            var tempModalDefaults = {windowClass: 'modal-fullscreen'};
+            var tempModalOptions = {};
+
+            //Map angular-ui modal custom defaults to modal defaults defined in service
+            angular.extend(tempModalDefaults, modalDefaults, customModalDefaults);
+
+            //Map modal.html $scope custom properties to defaults defined in service
+            angular.extend(tempModalOptions, modalOptions, customModalOptions);
+
+            if (!tempModalDefaults.controller) {
+                tempModalDefaults.controller = function ($scope, $uibModalInstance) {
+                    $scope.modalOptions = tempModalOptions;
+                    $scope.modalOptions.ok = function (result) {
+                        $uibModalInstance.close(result);
+                    };
+                    $scope.modalOptions.close = function (result) {
+                        $uibModalInstance.close();
+                    };
+                }
+            }
+
+            return $uibModal.open(tempModalDefaults).result;
+        };
+
+    }]);
