f0eeedc529da372f59f23ee875a4bc125700b9cc
Daniel Lauro <laurodaniel@outlook.com>
2018-11-26 21:58:58 -0300

    Delete platformOverrides.js

--- www/scripts/platformOverrides.js
+++
@@ -1,4 +0,0 @@
-﻿/*
-	Este archivo se reemplaza por código específico de la plataforma de la carpeta /merges.
-	Más información en http://taco.visualstudio.com/es-es/docs/configure-app/#Content.
-*/
