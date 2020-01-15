acbe46f6d16fc591ea8ada0febe8abd04940a423
wilmays10 <wilmays10@gmail.com>
2020-01-14 21:59:04 -0300

    cambio url_base de api de consulta worldline

--- www/js/cuantoTengoApp.js
+++ www/js/cuantoTengoApp.js
@@ -1,3 +1,3 @@
 var app = angular.module("cuantoTengo", ['ui.bootstrap', "angular-uuid"]);
 var url_destino = "https://gobiernoabierto.cordoba.gob.ar/api";
-var url_base = "https://200.123.180.122:5743";
+var url_base = "https://mrb.red-bus.com.ar:443";
